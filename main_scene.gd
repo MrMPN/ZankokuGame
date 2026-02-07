ac96b29b3fc2e7ff034d410d18c12a6f06b35cbe
Marc Portabella <portabella.marc@gmail.com>
2026-02-07 22:58:41 +0100

    improved structure of the project

--- main_scene.gd
+++ main_scene.gd
@@ -2,4 +2,10 @@ extends Node2D
 
 func _ready():
 	var title_screen = load("res://scenes/title/title_screen.tscn").instantiate()
+	title_screen.connect("start_pressed", _on_title_screen_start_pressed.bind(title_screen))
 	add_child(title_screen)
+
+func _on_title_screen_start_pressed(title_screen: Node):
+	title_screen.queue_free()
+	var character_scene = load("res://scenes/level1/level_1.tscn").instantiate()
+	add_child(character_scene)
--- scenes/title/title_screen.gd
+++ scenes/title/title_screen.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+signal start_pressed
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		start_pressed.emit()
