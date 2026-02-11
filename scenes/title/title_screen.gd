3a6ca3a8e0d6ffd9ef1fbf7c831df0006222a748
Marc Portabella <portabella.marc@gmail.com>
2026-02-11 01:28:36 +0100

    add sound effects, implement one as talking sound ans the other as space background noise (i know i know...)

--- scenes/title/title_screen.gd
+++ scenes/title/title_screen.gd
@@ -6,13 +6,16 @@ signal start_requested
 @onready var spaceship: Node2D = $Spaceship
 @onready var start_label: RichTextLabel = $CanvasLayer/Start
 @onready var music_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var intro_sound_player: AudioStreamPlayer = $IntroSoundPlayer
 
 var _is_starting: bool = false
 var _music_fade_tween: Tween = null
+var _intro_fade_tween: Tween = null
 var _manual_advance_backup: bool = true
 var _manual_advance_locked: bool = false
 
 @export var music_fade_duration: float = 3.0
+@export var intro_sound_fade_in_delay: float = 5.0
 @export var dialog_start_delay: float = 10.0
 @export var dialog_timeline_path: String = "res://dialogs/timeline.dtl"
 @export var auto_advance_only_for_intro: bool = true
@@ -20,6 +23,7 @@ var _manual_advance_locked: bool = false
 func _ready() -> void:
 	start_requested.connect(Callable(spaceship, "start_flight"))
 	spaceship.connect("flight_finished", _on_spaceship_flight_finished)
+	intro_sound_player.finished.connect(_on_intro_sound_finished)
 
 func _exit_tree() -> void:
 	if Dialogic and Dialogic.timeline_ended.is_connected(_on_dialogic_timeline_ended):
@@ -42,7 +46,7 @@ func _is_start_action(event: InputEvent) -> bool:
 	return is_ui_start or is_ui_accept
 
 func _start_intro_sequence() -> void:
-	_fade_out_music(music_fade_duration)
+	_crossfade_music_to_intro_sound(music_fade_duration)
 	await get_tree().create_timer(max(dialog_start_delay, 0.0)).timeout
 	if auto_advance_only_for_intro:
 		_lock_dialogic_manual_advance()
@@ -52,23 +56,54 @@ func _start_intro_sequence() -> void:
 	if auto_advance_only_for_intro and not Dialogic.timeline_ended.is_connected(_on_dialogic_timeline_ended):
 		Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
 
-func _fade_out_music(duration: float) -> void:
-	if not is_instance_valid(music_player) or not music_player.playing:
-		return
-
+func _crossfade_music_to_intro_sound(duration: float) -> void:
 	if is_instance_valid(_music_fade_tween):
 		_music_fade_tween.kill()
+	if is_instance_valid(_intro_fade_tween):
+		_intro_fade_tween.kill()
+
+	if is_instance_valid(intro_sound_player):
+		intro_sound_player.volume_db = -80.0
+		if intro_sound_player.playing:
+			intro_sound_player.stop()
 
 	if duration <= 0.0:
-		music_player.stop()
-		music_player.volume_db = 0.0
+		if is_instance_valid(music_player) and music_player.playing:
+			music_player.stop()
+			music_player.volume_db = 0.0
+		if is_instance_valid(intro_sound_player):
+			intro_sound_player.play()
+			intro_sound_player.volume_db = 0.0
 		return
 
 	_music_fade_tween = create_tween()
-	_music_fade_tween.tween_property(music_player, "volume_db", -80.0, duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	if is_instance_valid(music_player) and music_player.playing:
+		_music_fade_tween.parallel().tween_property(music_player, "volume_db", -80.0, duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+
 	_music_fade_tween.finished.connect(func() -> void:
-		music_player.stop()
-		music_player.volume_db = 0.0
+		if is_instance_valid(music_player) and music_player.playing:
+			music_player.stop()
+			music_player.volume_db = 0.0
+	)
+
+	if not is_instance_valid(intro_sound_player):
+		return
+
+	var fade_in_delay: float = max(0.0, intro_sound_fade_in_delay)
+	var fade_in_duration: float = max(0.05, duration - min(fade_in_delay, duration))
+	if fade_in_delay <= 0.0:
+		intro_sound_player.play()
+		_intro_fade_tween = create_tween()
+		_intro_fade_tween.tween_property(intro_sound_player, "volume_db", 0.0, fade_in_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+		return
+
+	var timer: SceneTreeTimer = get_tree().create_timer(fade_in_delay)
+	timer.timeout.connect(func() -> void:
+		if not _is_starting or not is_instance_valid(intro_sound_player):
+			return
+		intro_sound_player.play()
+		_intro_fade_tween = create_tween()
+		_intro_fade_tween.tween_property(intro_sound_player, "volume_db", 0.0, fade_in_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	)
 
 func _on_spaceship_flight_finished() -> void:
@@ -91,3 +126,7 @@ func _on_dialogic_timeline_ended() -> void:
 	if Dialogic.timeline_ended.is_connected(_on_dialogic_timeline_ended):
 		Dialogic.timeline_ended.disconnect(_on_dialogic_timeline_ended)
 	_unlock_dialogic_manual_advance()
+
+func _on_intro_sound_finished() -> void:
+	if _is_starting and is_instance_valid(intro_sound_player):
+		intro_sound_player.play()
